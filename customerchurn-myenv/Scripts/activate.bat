4dd7f49c9757036ea9129514243a005dac5882cf
nihalari <nihalari2001@gmail.com>
2025-04-04 12:29:41 +0530

    venv modified

--- customerchurn-myenv/Scripts/activate.bat
+++ customerchurn-myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\Project\project batch\Customer churn prediction\customerchurn-myenv
+set VIRTUAL_ENV=E:\Project\project batch\customer-churn-prediction\customerchurn-myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
